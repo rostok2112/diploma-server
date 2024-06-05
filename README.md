d509955bd4e1373492041a965eaafbc19718d872
little-endian <rostok21122112@gmail.com>
2024-06-05 08:51:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # diploma-server
 Server for reading, modifying and transfering data from FBT module to SteamVR
+
+---
+
+Server side of [FBT module project](https://github.com/rostok2112/diploma)
+
+
+Dependencies:
+- pipenv
+
+
+Initialization:
+
+```bash
+pipenv sync
+```
+
+Run:
+
+```bash
+pipenv run python src/main.py
+```
+
+Exit:
+
+`CTRL + C`
---
+++ src/main.py
@@ -0,0 +1,67 @@
+import asyncio
+from bleak import BleakScanner, BleakClient
+
+ 
+class BLDevice:
+    def __init__(self, name: str):
+        self.name = name
+        self.client = None
+        self.characteristic_uuid = None
+
+    async def connect(self):
+        if not self.client:
+            print(f"Trying to connect to {self.name} device...")
+            device = await BleakScanner.find_device_by_name(self.name, timeout=20.0)
+            if device is None:
+                raise Exception(f"Device with name {self.name} not found.")
+            self.client = BleakClient(device)
+            await self.client.connect()
+            print(f"Connected to {self.name}")
+            await self.client.pair()
+            print(f"Paired with {self.name}")
+
+    async def get_writable_char_uuid(self):
+        if not self.client:
+            await self.connect()
+        if not self.characteristic_uuid:
+            services = await self.client.get_services()
+            for service in services:
+                for char in service.characteristics:
+                    properties = set(char.properties)
+                    # in current circumstanse (original HM-10 ble module with latest firmware), 
+                    # in this way we can fiund a characteristic uuid that can be used for write
+                    if properties.issuperset({'read', 'write-without-response', 'write', 'notify', 'indicate'}):
+                        self.characteristic_uuid = char.uuid
+                        print(f"Found writable characteristic: {char.uuid}")
+            if not self.characteristic_uuid:
+                raise Exception("No writable characteristic found.")
+        return self.characteristic_uuid
+
+    async def write(self, message):
+        await self.connect()
+        try:
+            await self.client.write_gatt_char(
+                await self.get_writable_char_uuid(), 
+                message.encode('utf-8')
+            )
+        except Exception as e:
+            print("Got an error while writing: ", e)
+            return
+        print(f"Message '{message}' written to characteristic {self.characteristic_uuid}")
+
+    async def disconnect(self):
+        if self.client and self.client.is_connected:
+            await self.client.disconnect()
+            print(f"Disconnected from {self.name}")
+
+if __name__ == '__main__':
+    NAME = "baze"
+
+    my_device = BLDevice(NAME)
+    loop = asyncio.get_event_loop()
+
+    # Write to the characteristic
+    loop.run_until_complete(my_device.write("hello world!"))
+
+    # Disconnect (optional, good practice to disconnect when done)
+    loop.run_until_complete(my_device.disconnect())
